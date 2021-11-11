137889e4af08bdbcc04eedddb4768f1868b096a5
Rokon <rokon@genex.digital>
2021-11-11 18:51:58 +0600

    admin info changed

--- apps/webhook/admin.py
+++ apps/webhook/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Page, Ticket
 
+admin.site.site_header = "Social Attend Admin Panel"
+admin.site.site_title = "Social Attend Admin Portal"
+admin.site.index_title = "Welcome to Social Attend Portal"
+
 class PageAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name','source','page_id')
