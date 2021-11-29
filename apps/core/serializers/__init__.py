04dea82ddfc956c64f1fb93f2b88703bb2910919
Rokon <rokon@genex.digital>
2021-11-29 10:09:58 +0600

    merge conflict solved

--- apps/core/serializers/__init__.py
+++ apps/core/serializers/__init__.py
@@ -2,3 +2,4 @@ from .core_token_obtain_pair_serializer import CoreTokenObtainPairSerializer
 from .group_serializer import GroupSerializer
 from .texonomy_serializer import TexonomySerilizer
 from .user_serializer import UserSerializers
+from .register import RegisterSerializer
