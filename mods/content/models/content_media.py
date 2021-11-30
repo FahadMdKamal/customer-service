10034622217d2daea557858c75818beb1a75ddf6
nayonacademy <si.nayon@gmail.com>
2021-11-30 18:41:24 +0600

    media upload fix

--- mods/content/models/content_media.py
+++ mods/content/models/content_media.py
@@ -1,20 +1,19 @@
 from django.db import models
 
 
-class ContentMediaManager(models.Manager):
-    pass
-
-
 class ContentMedia(models.Model):
-    media_type = models.CharField(max_length=100)
-    media_ref = models.CharField(max_length=244)
-    public_url = models.FileField(upload_to='media/')
-    storage_provider = models.CharField(max_length=100)
-    status = models.CharField(max_length=244)
+    media_type = models.CharField(max_length=100, default="image")
+    media_ref = models.CharField(max_length=244, default="media/")
+    file = models.FileField(upload_to='media/')
+    storage_provider = models.CharField(max_length=100, default="local")
+    status = models.CharField(max_length=244, default="active")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    objects = ContentMediaManager()
+    def save(self, *args, **kwargs):
+        if not self.file:
+            self.media_ref = self.file
+        return super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'converse_content_media'
