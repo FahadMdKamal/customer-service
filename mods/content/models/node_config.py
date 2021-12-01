fa0ab5e1a0ca2652d99a0a45385945533ee16b31
nayonacademy <si.nayon@gmail.com>
2021-12-01 18:58:11 +0600

    conflict solve

--- mods/content/models/node_config.py
+++ mods/content/models/node_config.py
@@ -10,7 +10,7 @@ class NodeConfigManager(models.Manager):
 class NodeConfig(models.Model):
     flow_node = models.ForeignKey(FlowNode, on_delete=models.CASCADE, related_name='config', blank=True, null=True)
     key = models.CharField(max_length=244, null=True, blank=True)
-    value = models.JSONField(default={})
+    value = models.JSONField(default=dict)
     objects = NodeConfigManager()
 
     class Meta:
