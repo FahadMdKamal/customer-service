11cace492547ba87607fe990dd970e23eb9e1728
fahad-md-kamal <faahad.hossain@gmail.com>
2022-01-05 19:00:39 +0600

    Menu & Menu Item Create Api Wired Up

--- mods/content/views/content.py
+++ mods/content/views/content.py
@@ -44,32 +44,35 @@ class ContentCreateView(APIView):
             else:
                 content_parent_id = None
             # content save
-            content_create = Content.objects.filter(id=data["id"]).update(type_ref=data["content_type"],
-                                                                          app_id=data["app_id"],
-                                                                          title=data["title"],
-                                                                          subtitle=data["subtitle"],
-                                                                          description=data["description"],
-                                                                          default_action="",
-                                                                          action_items=data["action_items"],
-                                                                          parent_id=content_parent_id,
-                                                                          left_contents="",
-                                                                          display_order=data["display_order"],
-                                                                          content_body=data["content_body"],
-                                                                          content_format=data["content_format"],
-                                                                          template_cache="",
-                                                                          value_cache=""
-                                                                          )
-            # # content options save
-            # for key, value in data["options"].items():
-            #     content_options = ContentOptions(content=content_create,
-            #                                      option_name=key,
-            #                                      option_value=value)
-            #     content_options.save()
-            # # node content save
-            # node_content = NodeContent(flow_node_id=data["node_id"],
-            #                            content_id=content_create.id)
-            # node_content.save()
-            # # Content Media save
+            content_create = Content.objects.filter(id=data["id"])
+            content_create.update(
+                type_ref=data["content_type"],
+                app_id=data["app_id"],
+                title=data["title"],
+                subtitle=data["subtitle"],
+                description=data["description"],
+                default_action="",
+                action_items=data["action_items"],
+                parent_id=content_parent_id,
+                left_contents="",
+                display_order=data["display_order"],
+                content_body=data["content_body"],
+                content_format=data["content_format"],
+                template_cache="",
+                value_cache=""
+            )
+            # content options save
+            for key, value in data["options"].items():
+                content_options = ContentOptions(content=content_create.first(),
+                                                 option_name=key,
+                                                 option_value=value)
+                content_options.save()
+            # node content save
+            if data.get('node_id', None):
+                node_content = NodeContent(flow_node_id=data["node_id"],
+                                        content_id=content_create.id)
+                node_content.save()
+            # Content Media save
             return response.Response(data=data, status=status.HTTP_201_CREATED)
         else:
             content_parent_id = Content.objects.filter(pk=data['parent_id']).exists()
@@ -78,7 +81,7 @@ class ContentCreateView(APIView):
             else:
                 content_parent_id = None
             # content save
-            content_create = Content(type_ref=data["content_type"],
+            content_create = Content.objects.create(type_ref=data["content_type"],
                                      app_id=data["app_id"],
                                      title=data["title"],
                                      subtitle=data["subtitle"],
@@ -93,20 +96,19 @@ class ContentCreateView(APIView):
                                      template_cache="",
                                      value_cache=""
                                      )
-            content_create.save()
             # content options save
             for key, value in data["options"].items():
                 content_options = ContentOptions(content=content_create,
                                                  option_name=key,
                                                  option_value=value)
                 content_options.save()
+
             # node content save
-            try:
+            if data.get('node_id', None):
                 node_content = NodeContent(flow_node_id=data["node_id"],
                                            content_id=content_create.id)
                 node_content.save()
-            except:
-                pass
+
             # Content Media save
             data["id"] = content_create.id
             return response.Response(data=data, status=status.HTTP_201_CREATED)
@@ -114,34 +116,15 @@ class ContentCreateView(APIView):
 
 class SingleContentDetailsView(APIView):
 
-    def post(self, request):
-        results = {}
-        data = json.loads(request.body.decode('utf-8'))
-        id = data['id']
-        # content save
-        content_create = Content.objects.filter(id=id).first()
-        results.update({'id': id})
-        try:
-            results.update({"node_id": NodeContent.objects.filter(content_id=id).first().flow_node.id})
-        except:
-            pass
-        option = {}
-        for i in ContentOptions.objects.filter(content=id):
-            option.update({i.option_name: i.option_value})
-        results.update(options=option)
-        results.update(content_type=content_create.type_ref,
-                       app_id=content_create.app_id,
-                       title=content_create.title,
-                       subtitle=content_create.subtitle,
-                       description=content_create.description,
-                       action_items=content_create.action_items,
-                       parent_id=content_create.parent_id,
-                       display_order=content_create.display_order,
-                       content_body=content_create.content_body,
-                       content_format=content_create.content_format,
-                       )
+    def get(self, request):
+        params = {}
 
-        return response.Response(data=results, status=status.HTTP_201_CREATED)
+        if self.request.query_params.get("content-id", None) is not None:
+            params.update({"id": self.request.query_params["content-id"]})
+        content_obj = Content.objects.filter(**params)
+        if content_obj:
+            return Response(data=ContentSerializer(content_obj.first()).data, status=status.HTTP_200_OK)
+        return Response(data={"message" : "No Content Found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class ContentDeleteView(APIView):
@@ -157,3 +140,4 @@ class ContentDeleteView(APIView):
 
         else:
             return response.Response(status=404, data={"Content not found."})
+            
