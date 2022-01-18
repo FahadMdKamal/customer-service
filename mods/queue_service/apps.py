fb030b621043eca9499088824181bf3d4fa2aba0
Masuk Helal Anik <aniksharif@gmail.com>
2022-01-18 20:59:23 +0600

    Added queue topis taable

--- mods/queue_service/apps.py
+++ mods/queue_service/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class QueueServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'queue_service'
+    name = 'mods.queue_service'
---
+++ mods/queue_service/models/queue_topics.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+
+TOPICS=(
+    ('social','Social'),
+    ('email','Email'),
+    ('live_chat','Live_Chat'),
+    ('live_call','Live_Call'),
+    ('support','Support')
+)
+DIRECTION = (
+    ('fifo','FIFO'),
+    ('lifo','LIFO')
+)
+
+STATUS = (
+    ('open','OPEN'),
+    ('paused','PAUSED'),
+    ('closed','CLOSED'),
+    ('retired','RETIRED')
+)
+
+class QueueTopics(models.Model):
+    app_id = models.IntegerField(default=0)
+    topic = models.CharField(
+        max_length=20,
+        choices= TOPICS,
+        default='social',
+    )
+    strict_source=models.CharField(
+        max_length=100,
+        default='',
+        blank=True
+    )
+    push_direction =  models.CharField(
+        max_length=20,
+        choices= DIRECTION,
+        default='fifo',
+        blank=True
+    )
+    escalation_timeout = models.IntegerField(default=0,blank=True)
+    dispute_timeout = models.IntegerField(default=0,blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices= STATUS,
+        default='fifo',
+        blank=True
+    )
+    last_open_at = models.DateTimeField(null= True, blank=True)
+    last_closed_at = models.DateTimeField(null= True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    class Meta:
+        db_table = "mevrik_queue_topics"
+
