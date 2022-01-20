4b1fb7365f624ac45cb2122b5e08181d554ffc44
fahad-md-kamal <faahad.hossain@gmail.com>
2022-01-20 13:28:23 +0600

    - response Decoration modificed

--- apps/core/utils/api_response_decorator.py
+++ apps/core/utils/api_response_decorator.py
@@ -1,6 +1,8 @@
+import string
 from rest_framework.response import Response
+from rest_framework import status
 
-def decorate_response(serializer_data, status:bool, status_code, message) -> Response:
+def decorate_response(status:bool, status_code:status, message: string, serializer_data=None) -> Response:
     return Response({
         'status': "success" if status else "faild" ,
         'message': message,
