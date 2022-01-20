44655e5b8dea65bf04671e691668dc390179c89f
navid <navid.chowdhury@upaybd.com>
2022-01-20 14:04:34 +0600

    added celery tasks

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 import os
-
+from django_mail_admin.models import Mailbox
 from celery import Celery
 from django.conf import settings
 # Set the default Django settings module for the 'celery' program.
@@ -16,6 +16,18 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+def fetch_mail():
+    qs = Mailbox.objects.iterator()
+    for mailbox in qs:
+        mailbox.get_new_mail()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, fetch_mail(), name='add every 10')
 
 
 @app.task(bind=True)
--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,4 @@ urlpatterns = [
     path('casex/', include('apps.casex.urls')),
     path('content/', include('mods.content.urls')),
     path('nlu/', include('mods.nlu.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
