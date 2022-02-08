9ff9e5090bfb63fcc69473ee591b2b33c108fd8d
fahad-md-kamal <faahad.hossain@gmail.com>
2022-02-08 16:48:39 +0600

    Extractor Error Handled

--- mods/nlu/views/extractor.py
+++ mods/nlu/views/extractor.py
@@ -7,10 +7,13 @@ class ExtratorViewSet(ViewSet):
 
     def create(self, request):
         params = []
-        for k, v in request.data.items():
-            if k == "func":
-                continue
-            params.append(v)
-        extractor = functions.Extractors()
-        func = getattr(extractor, request.data.get('func'))(*params)
-        return Response({"result": func })
+        try:
+            for k, v in request.data.items():
+                if k == "func":
+                    continue
+                params.append(v)
+            extractor = functions.Extractors()
+            func = getattr(extractor, request.data.get('func'))(*params)
+            return Response({"result": func })
+        except:
+            return Response({"result": 'Invalid Try' }, status=400)
