ceffb1779396fc55944aff63a540b29503966e65
fahad-md-kamal <faahad.hossain@gmail.com>
2022-02-13 17:47:59 +0600

    Flow Node Bug Fixed

--- mods/content/serializers/flow_node_all_serializers.py
+++ mods/content/serializers/flow_node_all_serializers.py
@@ -5,18 +5,18 @@ from mods.content.models import FlowNode
 
 class FlowNodeAllSerializer(ModelSerializer):
     # mevrik = NodeConfigSerializer(many=True)
-    config = NodeConfigSerializer(many=True)
+    # config = NodeConfigSerializer(many=True)
 
     class Meta:
         model = FlowNode
-        fields = ('id', 'name', 'flow', 'node_type', "mevrik", "content_type", "initial_content_id", "config")
+        fields = ('id', 'name', 'flow', 'node_type', "mevrik", "content_type", "initial_content_id")
         depths = -1
 
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        if ret["node_type"] == "start":
-            ret.pop("content_type")
-            ret.pop("initial_content_id")
+    # def to_representation(self, instance):
+    #     ret = super().to_representation(instance)
+    #     if ret["node_type"] == "start":
+    #         ret.pop("content_type")
+    #         ret.pop("initial_content_id")
 
-        return ret
+    #     return ret
 
